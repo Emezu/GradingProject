1bf367a2b72fdfa33e25603833416fad8281ffe6
Emezu <emezufabian@gmail.com>
2026-03-17 21:53:32 +0100

    Created a loop

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,24 @@ public class Main{
 
         System.out.println("How many students? ");
         int count = scanner.nextInt();
-}
+        Student[] students = new Student[count];
+
+        for (int i = 0; i < count; i++) {
+            students[i] = new Student();
+
+            System.out.println("What is the student name? ");
+            students[i].name = scanner.next();
+
+            System.out.println("What is the student's score? ");
+            students[i].score = scanner.nextDouble();
+        }
+        System.out.println("\nName     Score     Grade");
+        System.out.println("___________________________");
+
+        for (int i = 0; i < count; i++) {
+            System.out.printf("%-12s%-8.1f%s%n", students[i].name, students[i].score, students[i].getGrade());
+
+
+        }
+    }
 }
